b56f8112d2e66460bf7c93c64d384a274a1f8485
Evgeniy Khramkov <ewgeniux@gmail.com>
2017-04-04 16:38:28 +0300

    init

---
+++ spec/counter_action_spec.rb
@@ -0,0 +1,9 @@
+describe Fastlane::Actions::CounterAction do
+  describe '#run' do
+    it 'prints a message' do
+      expect(Fastlane::UI).to receive(:message).with("The counter plugin is working!")
+
+      Fastlane::Actions::CounterAction.run(nil)
+    end
+  end
+end
