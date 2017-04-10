8f85f9a65a07a38a3a0790f4ec775b95b321e98d
Evgeniy Khramkov <ewgeniux@gmail.com>
2017-04-10 09:43:19 +0300

    removing counter action

--- spec/counter_action_spec.rb
+++
@@ -1,8 +0,0 @@
-describe Fastlane::Actions::CounterAction do
-  describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The counter plugin is working!")
-      Fastlane::Actions::CounterAction.run(nil)
-    end
-  end
-end
